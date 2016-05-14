946e7b735e3cd47fec6d56b1fa68e9dddd7bb647
sora <specialzack@yahoo.com>
2016-05-13 23:55:07 -0400

    change settings.py allowed host

--- unbrokeme/settings.py
+++ unbrokeme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zz42q#6%&w@*28qifm@_#ms7kpti5+uwk9$4q=urwp@#t+md^b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com",]
 
 
 # Application definition
