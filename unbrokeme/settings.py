19a9e235e54794405911f15c391c3d8c1124cb11
sora <specialzack@yahoo.com>
2016-05-13 23:50:56 -0400

    change Procfile, requirements.txt, settings.py and wsgi.py

--- unbrokeme/settings.py
+++ unbrokeme/settings.py
@@ -119,6 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
