08e83354085c18cce7aaa1cfb0143219fed4fb6c
sora <specialzack@yahoo.com>
2016-05-14 20:22:41 -0400

    change get total condition

--- unbroke/dbconnect.py
+++ unbroke/dbconnect.py
@@ -453,7 +453,7 @@ def getdtotal(user, year, month):
         total.append(c.fetchone())
     except ProgrammingError:
         total.append((0,))
-    if total[0][0] = None:
+    if total[0][0] == None:
         total.append((0,))
     conn.close()
     return total[0]
@@ -475,7 +475,7 @@ def getdtotal2(userID, year, month):
         total.append(c.fetchone())
     except ProgrammingError:
         total.append((0,))
-    if total[0][0] = None:
+    if total[0][0] == None:
         total.append((0,))
     conn.close()
     return total[0]
@@ -497,7 +497,7 @@ def getetotal(user, year, month):
         total.append(c.fetchone())
     except ProgrammingError:
         total.append((0,))
-    if total[0][0] = None:
+    if total[0][0] == None:
         total.append((0,))
     conn.close()
     return total[0]
@@ -519,7 +519,7 @@ def getetotal2(userID, year, month):
         total.append(c.fetchone())
     except ProgrammingError:
         total.append((0,))
-    if total[0][0] = None:
+    if total[0][0] == None:
         total.append((0,))
     conn.close()
     return total[0]
@@ -541,7 +541,7 @@ def getsavings(user, year, month):
         total.append(c.fetchone())
     except ProgrammingError:
         total.append((0,))
-    if total[0][0] = None:
+    if total[0][0] == None:
         total.append((0,))
     conn.close()
     return total
